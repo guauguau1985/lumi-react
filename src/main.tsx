d2b0273811b7f734bc2fbff118f62ae2415fa1e1
guauguau1985 <katerinepadillamaya@gmail.com>
2025-09-23 22:56:32 -0300

    fix: render AppShell desde App

--- src/main.tsx
+++ src/main.tsx
@@ -1,33 +1,10 @@
-//________Envolver App__________________
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import AppShell from "./AppShell";  
-import './styles/global.css'
-import { ProgressProvider } from "./state/progress";
-import { MoodProvider } from "./state/mood";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
 import "./index.css";
 
-
-console.log("[boot]", window.location.href);
-
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
-)
-
-// ...Progresos y desbloqueos___________________
-root.render(
-  <React.StrictMode>
-    <ProgressProvider>
-      <App />
-    </ProgressProvider>
   </React.StrictMode>
 );
-
-// ...Reacciones de lumi__________________________
-<ProgressProvider>
-  <MoodProvider>
-    <App />
-  </MoodProvider>
-</ProgressProvider>
