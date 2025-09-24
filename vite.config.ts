3833870cc0b71dd18fc97cd33616287a6ad7bf26
guauguau1985 <katerinepadillamaya@gmail.com>
2025-09-24 09:17:05 -0300

    fix: add vite-tsconfig-paths (alias @/) for build

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
+// vite.config.ts
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
-  base: "/lumi-react/",
+  base: "/lumi-react/",   // necesario para GitHub Pages
 });
