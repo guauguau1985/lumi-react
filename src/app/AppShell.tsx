55456dae9e920bc8e277787125a8db0e56a05c37
guauguau1985 <katerinepadillamaya@gmail.com>
2025-10-22 14:58:24 -0300

    feat: avance X

--- src/app/AppShell.tsx
+++ src/app/AppShell.tsx
@@ -2,16 +2,14 @@
 import { PropsWithChildren } from "react";
 import { MoodProvider } from "@/state/mood";
 import { ProgressProvider } from "@/state/progress";
-import { GamificationProvider } from "@/state/progress"; // si está aquí, ajusta el import real
+
 
 export function AppProviders({ children }: PropsWithChildren) {
   return (
     <ProgressProvider>
       <MoodProvider>
-        <GamificationProvider>
-          {children}
-        </GamificationProvider>
-      </MoodProvider>
+                 {children}
+        </MoodProvider>
     </ProgressProvider>
   );
 }
